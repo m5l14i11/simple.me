28f76ed34d0866e68da733c8a91004c9b44d4252
Siarhei Melnik <siarhei.m.a@gmail.com>
2020-08-14 12:47:07 +0300

    update infra

--- bin/me.js
+++ bin/me.js
@@ -1,7 +1,26 @@
 #!/usr/bin/env node
 
 const cdk = require('@aws-cdk/core');
-const { MeStack } = require('../lib/me-stack');
+const { MeSite } = require('../lib/me-site');
+
+class MeStack extends cdk.Stack {
+    constructor(parent, name, props) {
+        super(parent, name, props);
+
+        new MeSite(this, 'MeSite', {
+            domainName: this.node.tryGetContext('domain'),
+            siteSubDomain: this.node.tryGetContext('subdomain'),
+        });
+   }
+}
 
 const app = new cdk.App();
-new MeStack(app, 'MeStack');
+
+new MeStack(app, 'MeStack', {
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT, 
+        region: process.env.CDK_DEFAULT_REGION
+    }
+});
+
+app.synth();
