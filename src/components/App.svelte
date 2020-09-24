4768b05b6d9d11713dfaf500d94d7215cc5a768e
Siarhei Melnik <siarhei.m.a@gmail.com>
2020-09-24 15:10:46 +0300

    feat: small change

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,9 +1,23 @@
 <script>
 	export let name;
+	export let intro;
+	export let socials;
+	export let outro;
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
+	<h2>{intro}</h2>
+	<ul>
+		{#each socials as item, i}
+			<li>
+				<a target="_blank" href="{item.link}">
+					{item.name}
+				</a>
+			</li>
+		{/each}
+	</ul>
+	<div>{outro}</div>
 </main>
 
 <style>
@@ -19,6 +33,30 @@
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
+	}
+
+	h2 {
+		color: #ff3100b5;
+		text-transform: capitalize;
+		font-size: 3em;
+		font-weight: 100;
+	}
+	
+	ul {
+		list-style-type: none;
+  		margin: 0;
+  		padding: 0;
+	}
+
+	a {
+		color: #ff0000;
+		text-transform: capitalize;
+		font-size: 1.5em;
+		font-weight: 100;
+	}
+
+	div {
+		color: #fff;
 	}
 
 	@media (min-width: 640px) {
--- src/main.js
+++ src/main.js
@@ -2,7 +2,29 @@ import App from './components/App.svelte';
 
 const app = new App({
 	target: document.body,
-	props: { name: 'Username' }
+	props: {
+		name: 'EveryOne',
+		intro: 'I am Siarhei Melnik, Software Engineer',
+		socials: [
+			{
+				name: 'email',
+				link: 'mailto:siarhei.m.a@gmail.com'
+			},
+			{
+				name: 'twitter',
+				link: 'https://twitter.com/m_siarhei'
+			},
+			{
+				name: 'github',
+				link: 'https://github.com/m5l14i11'
+			},
+			{
+				name: 'telegram',
+				link: 'https://t.me/msiarhei'
+			}
+		].sort((a, b) => b.name.length - a.name.length),
+		outro: 'Wm05c2JHOTNJSFJvWlNCM2FHbDBaU0J5WVdKaWFYUWc4SitRaHc9PQ==' 
+	}
 });
 
 export default app;
