20f98b914e10c8961386199e67bfee8dda8933e3
Siarhei Melnik <siarhei.m.a@gmail.com>
2021-02-11 23:24:27 +0300

    100

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -5,51 +5,36 @@
 	export let outro;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<h2>{intro}</h2>
-	<ul>
-		{#each socials as item}
-			<li>
-				<a target="_blank" rel="noopener" href="{item.link}">
-					{item.name}
-				</a>
-			</li>
-		{/each}
-	</ul>
-	<div>{outro}</div>
-</main>
-
 <style>
 	main {
 		text-align: center;
-		padding: 1em;
-		max-width: 240px;
+		padding: 2em;
+		width: 100%;
 		margin: 0 auto;
 	}
 
 	h1 {
-		color: #ff3e00;
+		color: #de3600;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
 	}
 
 	h2 {
-		color: #ff3100b5;
+		color: #de3600;
 		text-transform: capitalize;
 		font-size: 3em;
 		font-weight: 100;
 	}
-	
+
 	ul {
 		list-style-type: none;
-  		margin: 0;
-  		padding: 0;
+		margin: 0;
+		padding: 0;
 	}
 
 	a {
-		color: #ff0000;
+		color: #ee0000;
 		text-transform: capitalize;
 		font-size: 1.5em;
 		font-weight: 100;
@@ -61,7 +46,23 @@
 
 	@media (min-width: 640px) {
 		main {
-			max-width: none;
+			max-width: 100%;
+			text-align: center;
 		}
 	}
 </style>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<h2>{intro}</h2>
+	<ul>
+		{#each socials as item}
+			<li>
+				<a target="_blank" rel="noopener" href={item.link}>
+					{item.name}
+				</a>
+			</li>
+		{/each}
+	</ul>
+	<div>{outro}</div>
+</main>
